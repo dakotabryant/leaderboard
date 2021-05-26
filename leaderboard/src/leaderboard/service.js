619b09c070af2bc6b2c8405a9c96566cc16361f2
Dakota Bryant <bryant.dakota@gmail.com>
2021-05-26 00:20:42 -0400

    added the shellRacer service method, made some reusable constants

--- leaderboard/src/leaderboard/service.js
+++ leaderboard/src/leaderboard/service.js
@@ -1,11 +1,12 @@
 import { HttpError } from '../errors'
 
+const mode = 'cors'
+const headers = {
+  'Content-Type': 'application/json',
+}
+const BASE_URL = process.env.REACT_APP_API_URL
 export const getLeaderboard = async () => {
-  const url = `${process.env.REACT_APP_API_URL}/leaderboards/racers`
-  const mode = 'cors'
-  const headers = {
-    'Content-Type': 'application/json'
-  }
+  const url = `${BASE_URL}/leaderboards/racers`
   try {
     const res = await fetch(url, { headers, mode })
 
@@ -15,6 +16,20 @@ export const getLeaderboard = async () => {
 
     const leaderboard = await res.json()
     return leaderboard
+  } catch (e) {
+    console.error(e)
+    return []
+  }
+}
+
+export const shellRacer = async (racerName) => {
+  // We have to split/join the text due to the slugify expectation on the server side for names with spaces like "Shy Guy". encodeURIComponent() will not suffice here
+  const url = `${BASE_URL}/racers/${racerName.split(' ').join('-')}/shell`
+  try {
+    const res = await fetch(url, { headers, mode, method: 'POST' })
+    if (!res.ok) {
+      throw new HttpError(res)
+    }
   } catch (e) {
     console.error(e)
     return []
