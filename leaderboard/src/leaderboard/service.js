a2674091f981f37482d22f1d33d840c2f744d68d
Dakota Bryant <bryant.dakota@gmail.com>
2021-05-26 00:22:01 -0400

    being consistent with my naming conventions because that's cool

--- leaderboard/src/leaderboard/service.js
+++ leaderboard/src/leaderboard/service.js
@@ -1,14 +1,14 @@
 import { HttpError } from '../errors'
 
-const mode = 'cors'
-const headers = {
+const MODE = 'cors'
+const HEADERS = {
   'Content-Type': 'application/json',
 }
 const BASE_URL = process.env.REACT_APP_API_URL
 export const getLeaderboard = async () => {
   const url = `${BASE_URL}/leaderboards/racers`
   try {
-    const res = await fetch(url, { headers, mode })
+    const res = await fetch(url, { HEADERS, MODE })
 
     if (!res.ok) {
       throw new HttpError(res)
@@ -26,7 +26,7 @@ export const shellRacer = async (racerName) => {
   // We have to split/join the text due to the slugify expectation on the server side for names with spaces like "Shy Guy". encodeURIComponent() will not suffice here
   const url = `${BASE_URL}/racers/${racerName.split(' ').join('-')}/shell`
   try {
-    const res = await fetch(url, { headers, mode, method: 'POST' })
+    const res = await fetch(url, { HEADERS, MODE, method: 'POST' })
     if (!res.ok) {
       throw new HttpError(res)
     }
